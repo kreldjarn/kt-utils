04ebad3f8163629e11d8c39f6dc0745104e4fb57
Kristján Eldjárn <kristjan@eldjarn.net>
2017-05-27 12:57:07 +0000

    Filled in function to parse date from kt

--- js/kt_utils.js
+++ js/kt_utils.js
@@ -56,7 +56,7 @@ exports = (function() {
   public.get_inception_date(kt) {
     if (!public.is_valid(kt)) throw 'Illegal kennitala.';
     var year = {8: 1800, 9: 1900, 0: 2000, 1: 2100}[kt.substr(9, 1)] + kt.substr(4, 2);
-
+    return new Date(year, -1 + kt.substr(2, 2), kt.substr(0, 2));
   }
 
   return public;
