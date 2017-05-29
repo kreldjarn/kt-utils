5b81bb9e4e8de288b24c34df5b871d212901e9b3
Kristján Eldjárn <kristjan@eldjarn.net>
2017-05-29 17:35:05 +0000

    public -> kt_utils

--- js/kt_utils.js
+++ js/kt_utils.js
@@ -1,5 +1,5 @@
 module.exports = (function() {
-  var public = {};
+  var kt_utils = {};
 
   function _strip_dash(kt) {
     return kt.replace(/-/g, "");
@@ -24,7 +24,7 @@ module.exports = (function() {
     return in_array;
   }
 
-  public.is_valid = function(kt) {
+  kt_utils.is_valid = function(kt) {
     kt = _strip_dash(kt);
 
     if (!/^\d{10}$/.test(kt)) return false;
@@ -32,13 +32,13 @@ module.exports = (function() {
     if (!_in_array(_get_valid_centuries(), kt.substr(-1))) return false;
 
     try {
-      return public.calculate_checksum == kt.substr(8, 1);
+      return kt_utils.calculate_checksum == kt.substr(8, 1);
     } catch {
       return false;
     }
   }
 
-  public.calculate_checksum = function(kt) {
+  kt_utils.calculate_checksum = function(kt) {
     var weights = [3, 2, 7, 6, 5, 4, 3, 2];
     kt = kt.substr(0, 8);
     if (kt.length < 8) throw 'Checksum requires 8 digits to calculate';
@@ -53,16 +53,16 @@ module.exports = (function() {
     return (11 - modulus) % 11;
   }
 
-  public.get_inception_date = function(kt) {
-    if (!public.is_valid(kt)) throw 'Illegal kennitala.';
+  kt_utils.get_inception_date = function(kt) {
+    if (!kt_utils.is_valid(kt)) throw 'Illegal kennitala.';
     var year = {8: 1800, 9: 1900, 0: 2000, 1: 2100}[kt.substr(9, 1)] + kt.substr(4, 2);
     return new Date(year, -1 + kt.substr(2, 2), kt.substr(0, 2));
   }
 
-  public.get_entity_type = function(kt) {
-    if (!public.is_valid(kt)) throw 'Illegal kennitala.';
+  kt_utils.get_entity_type = function(kt) {
+    if (!kt_utils.is_valid(kt)) throw 'Illegal kennitala.';
     return ['individual', 'company'][Math.floor(kt.substr(0,1) / 4)];
   }
 
-  return public;
+  return kt_utils;
 })();
