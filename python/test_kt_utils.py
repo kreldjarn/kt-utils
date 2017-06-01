7deee48693c947f1c082d0efc0455a055274f209
Kristján Eldjárn <kristjan@eldjarn.net>
2017-06-01 23:32:10 +0000

    Wrote a test…

--- python/test_kt_utils.py
+++ python/test_kt_utils.py
@@ -1,8 +1,17 @@
-from unittest import TestCase
+import unittest
 import time
 
 import kt_utils
 
 
-class TestKtUtils(TestCase):
-    pass
+class TestKtUtils(unittest.TestCase):
+    def test_is_invalid(self):
+        # Too short
+        self.assertFalse(kt_utils.is_valid('111111'))
+        # Non-integer string
+        self.assertFalse(kt_utils.is_valid('1A2B3C4D5E'))
+        # Fails checksum
+        self.assertFalse(kt_utils.is_valid('1101115040'))
+
+if __name__ == '__main__':
+    unittest.main()
